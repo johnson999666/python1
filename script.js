c5e2d2409609c2e03bed0b3dbea1084b843723ba
johnson999666 <123405062+johnson999666@users.noreply.github.com>
2023-04-19 05:07:19 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-var mySwiper = new Swiper('.swiper-container', {
-    direction: 'horizontal',
-    loop: true,
-    pagination: {
-      el: '.swiper-pagination',
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-  
+
+// Initialize Swiper
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  touchEventsTarget: 'wrapper',
+});
